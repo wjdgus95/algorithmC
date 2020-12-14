16bfcd00bd1f0d7a7b5e74dbed90e4012a9f25a8
wjdgus95 <wjdguswjdal@naver.com>
2020-12-14 20:46:35 +0900

    가장큰수

--- Algorithm/nextpermutation.cpp
+++ Algorithm/nextpermutation.cpp
@@ -1,25 +1,26 @@
 #pragma warning(disable: 4996)
+#include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-string solution(vector<int> numbers) { //가장 큰 수 - fail
+bool cmp(const string& a, const string& b) { //string으로 비교 수행
+	return a + b > b + a;
+}
+
+string solution(vector<int> numbers) { //가장 큰 수 - success
+	string answer = "";
 	vector<string> tmp;
 
-	sort(numbers.begin(), numbers.end());
+	for (int i = 0; i < numbers.size(); i++) tmp.push_back(to_string(numbers[i]));
 
-	do {
-		string s = "";
-		for (int i = 0; i < numbers.size(); i++) {
-			s += to_string(numbers[i]);
-		}
-		tmp.push_back(s);
+	sort(tmp.begin(), tmp.end(), cmp); //cmp 함수 기준으로 정렬 수행
 
-	} while (next_permutation(numbers.begin(), numbers.end()));
+	for (int i = 0; i < tmp.size(); i++) answer += tmp[i];
 
-	sort(tmp.begin(), tmp.end());
+	if (answer[0] == '0') return "0"; //answer[0]가 0이면 가장 큰 수가 0이라는 뜻이므로 0리턴
 
-	return tmp[tmp.size() - 1];
+	return answer;
 }
